f260ca44ea5dee18e5e91a47576641a9927f7752
Darryl Whitmore <darryl.whitmore@gmail.com>
2019-09-29 13:31:06 -0500

    implement basic configuration file

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using Microsoft.Extensions.Configuration;
 
 namespace PlexMusicPlaylistExporter {
 	class Program {
 		static void Main( string[] args ) {
-			Console.WriteLine( "Hello World!" );
+			IConfiguration config = new ConfigurationBuilder()
+				.AddJsonFile( "appsettings.json", true, true )
+				.Build();
+
+			Console.WriteLine( $"IP: {config["plexIp"]}" );
+			Console.WriteLine( $"token: {config["plexToken"]}" );
 		}
 	}
 }
