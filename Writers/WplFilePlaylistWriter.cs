349749f1842d380ece98eadd292f9a2ddfd8b839
Darryl Whitmore <darryl.whitmore@gmail.com>
2019-10-14 17:14:40 -0500

    Windows Media Player Playlist (WPL) support

---
+++ Writers/WplFilePlaylistWriter.cs
@@ -0,0 +1,71 @@
+﻿using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+using PlexPlaylistExporter.WriteSupport;
+
+namespace PlexMusicPlaylistExporter.Writers {
+	internal class WplFilePlaylistWriter : FilePlaylistWriter {
+		private readonly string generatorValue;
+
+		public WplFilePlaylistWriter( string destinationFolder, string generatorValue ) : base( destinationFolder ) {
+			this.generatorValue = generatorValue;
+		}
+
+		public override void Write( XElement mediaContainerElement ) {
+			var playlistTitle = GetPlaylistTitle( mediaContainerElement );
+
+			var tracks = mediaContainerElement.Elements( "Track" ).ToList();
+
+			var destinationFileName = $"{SanitizeFileName( playlistTitle )}.wpl";
+
+			var writerSettings = new XmlWriterSettings {
+				Indent = true,
+				OmitXmlDeclaration = true
+			};
+
+			using ( var writer = XmlWriter.Create( Path.Combine( DestinationFolder, destinationFileName ), writerSettings ) ) {
+				writer.WriteStartDocument();
+
+				writer.WriteRaw( "<?wpl version=\"1.0\"?>" );
+				writer.WriteRaw( "\n" );
+
+				writer.WriteStartElement( "smil" );
+
+				writer.WriteStartElement( "head" );
+
+				writer.WriteStartElement( "meta" );
+				writer.WriteAttributeString( "name", "Generator" );
+				writer.WriteAttributeString( "content", generatorValue );
+				writer.WriteEndElement();
+
+				writer.WriteStartElement( "meta" );
+				writer.WriteAttributeString( "name", "ItemCount" );
+				writer.WriteAttributeString( "content", tracks.Count.ToString() );
+				writer.WriteEndElement();
+
+				writer.WriteElementString( "title", playlistTitle );
+				writer.WriteEndElement();
+
+				writer.WriteStartElement( "body" );
+
+				writer.WriteStartElement( "seq" );
+				foreach ( var track in tracks ) {
+					var audioFile = track.Element( "Media" )?.Element( "Part" )?.Attribute( "file" )?.Value;
+
+					if ( audioFile != null ) {
+						writer.WriteStartElement( "media" );
+						writer.WriteAttributeString( "src", audioFile );
+						writer.WriteEndElement();
+					}
+				}
+				writer.WriteEndElement();
+
+				writer.WriteEndElement();
+
+				writer.WriteEndElement();
+				writer.WriteEndDocument();
+			}
+		}
+	}
+}
